5b3a9acec7590df780a126472e2b9b5ea5300955
saranshbarua <saranshbarua@gmail.com>
2020-05-10 22:32:20 +0530

    Configure webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,12 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
   plugins: [new CleanWebpackPlugin()],
   module: {
     rules: [
