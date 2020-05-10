41af6bcc4b20d7307c0a9fd0bbd17591548840d4
saranshbarua <saranshbarua@gmail.com>
2020-05-10 22:25:09 +0530

    Clean /dist on each build with clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   devtool: "inline-source-map",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
+  plugins: [new CleanWebpackPlugin()],
   module: {
     rules: [
       {
@@ -14,5 +12,9 @@ module.exports = {
         use: ["file-loader"]
       }
     ]
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   }
 };
